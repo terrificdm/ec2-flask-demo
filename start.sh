582a7698413a5dec3ff7b494984b3a8894894dc7
Guo <guodaomi@38f9d354dd17.ant.amazon.com>
2021-07-07 18:21:40 +0800

    add .sh profile

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Install pip, Flask and git
+yum update -y
+yum install pip -y
+pip install Flask==1.1.2
+yum install git -y
+# Download app and run it
+git clone https://github.com/terrificdm/ec2-flask-demo
+cd ec2-flask-demo
+nohup python app.py >/dev/null 2>&1 &
